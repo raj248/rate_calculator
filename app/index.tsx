aae55c09b8324c7a60ccbd9ed366b82f7d44e8ed
raj248 <shashankraj007281@gmail.com>
2026-07-31 22:47:39 +0530

    bug fixes

--- app/index.tsx
+++ app/index.tsx
@@ -5,10 +5,7 @@ import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import { Icon } from 'react-native-paper';
 import AddEntryPopup from '~/components/UpsertEntryPopup';
 
-
 export default function Screen() {
-  const [date, setDate] = useState<string>("");
-  const [openSheet, setOpenSheet] = useState<(() => void) | null>(null);
   const insets = useSafeAreaInsets(); // Get safe area padding
   const [showUpsertPopup, setShowUpsertPopup] = useState(false);
 
@@ -17,16 +14,14 @@ export default function Screen() {
       <DataList />
 
       <TouchableOpacity
-        onPress={() => setShowUpsertPopup(true)} className="absolute right-6 bg-blue-600 w-14 h-14 rounded-full items-center justify-center shadow-lg"
+        onPress={() => setShowUpsertPopup(true)}
+        className="absolute right-6 h-14 w-14 items-center justify-center rounded-full bg-blue-600 shadow-lg"
         style={{ bottom: insets.bottom + 20 }} // Adjust for navigation bar
       >
         <Icon source={'plus'} color="white" size={28} />
       </TouchableOpacity>
 
-      <AddEntryPopup
-        visible={showUpsertPopup}
-        onDismiss={() => setShowUpsertPopup(false)}
-      />
+      <AddEntryPopup visible={showUpsertPopup} onDismiss={() => setShowUpsertPopup(false)} />
     </View>
   );
 }
--- components/Settings.tsx
+++ components/Settings.tsx
@@ -1,8 +1,9 @@
-import { View } from 'react-native';
+import { TouchableOpacity, View } from 'react-native';
 import { Text } from './nativewindui/Text';
 import React from 'react';
 import { useSettingsStore } from '~/store/settingsStore';
 import { Slider } from './nativewindui/Slider';
+import { useRateStore } from '~/store/rateStore';
 
 export default function SettingsScreen() {
   const textSize = useSettingsStore((state) => state.textSize);
@@ -10,7 +11,7 @@ export default function SettingsScreen() {
 
   return (
     <View className="p-4">
-      <Text className="text-lg font-bold mb-2">Text Size</Text>
+      <Text className="mb-2 text-lg font-bold">Text Size</Text>
       <Slider
         minimumValue={10}
         maximumValue={40}
@@ -19,6 +20,14 @@ export default function SettingsScreen() {
         onValueChange={(value) => setTextSize(value)}
       />
       <Text className="mt-2 text-center">{`Current Text Size: ${textSize}px`}</Text>
+      {/* Clear Data Button */}
+      <TouchableOpacity
+        className="mt-4 rounded-md bg-red-500 p-2"
+        onPress={() => {
+          useRateStore.setState({ data: {}, totals: {} });
+        }}>
+        <Text className="text-center text-white">Clear Data</Text>
+      </TouchableOpacity>
     </View>
   );
 }
